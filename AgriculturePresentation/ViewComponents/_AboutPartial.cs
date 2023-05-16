6d3658efc134793ca27fc609d8d05fa627338aa8
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-05-16 11:50:22 +0300

    EntityLayer About sınıfı eklendi ve gerekli işlemler yapıldı. _AboutPartial ile viewcomponentler düzenlendi.

---
+++ AgriculturePresentation/ViewComponents/_AboutPartial.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class _AboutPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            AgricultureContext c= new AgricultureContext();
+            var values = c.Abouts.ToList();
+            return View(values);
+        }
+    }
+}
