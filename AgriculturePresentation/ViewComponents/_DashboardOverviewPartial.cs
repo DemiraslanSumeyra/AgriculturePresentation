492bd310888afa03761bd4e7c25a1cb555793498
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-05-17 15:55:46 +0300

    UI kısmında Dashboard kısmı düzenlendi. Ona bağlı Viewcomponenetler düzenlendi.

---
+++ AgriculturePresentation/ViewComponents/_DashboardOverviewPartial.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class _DashboardOverviewPartial:ViewComponent
+    {
+        AgricultureContext c = new AgricultureContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.teamCount = c.Teams.Count();
+            ViewBag.serviceCount = c.Services.Count();
+            ViewBag.messageCount = c.Contacts.Count();
+            ViewBag.currentMonthMessage = c.Contacts.Where(x => x.Date.Month == DateTime.Now.Month).Count();
+
+            ViewBag.announcementTrue = c.Announcements.Where(x => x.Status == true).Count();
+            ViewBag.announcementFalse = c.Announcements.Where(x => x.Status == false).Count();
+
+            ViewBag.urunYonetimi = c.Teams.Where(x => x.Title == "Süt Ürünleri Yöneticisi").Select(y => y.PersonName).FirstOrDefault();
+            ViewBag.sutUretici = c.Teams.Where(x => x.Title == "Süt Ürünleri Üreticisi").Select(y => y.PersonName).FirstOrDefault();
+            ViewBag.bakliyatYonetimi = c.Teams.Where(x => x.Title == "Bakliyat Yönetimi").Select(y => y.PersonName).FirstOrDefault();
+            ViewBag.urunPazarlama = c.Teams.Where(x => x.Title == "Ürün Pazarlama").Select(y => y.PersonName).FirstOrDefault();
+            return View();
+        }
+    }
+}
