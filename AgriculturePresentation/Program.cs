df19f024c7f407bfb05de0e64b54d28d0582094d
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-05-24 17:00:27 +0300

    BusinessLayer  Container klasörü oluşturuldu. Konfigurasyon işlemleri Extensions classına taşındı.

--- AgriculturePresentation/Program.cs
+++ AgriculturePresentation/Program.cs
@@ -8,29 +8,17 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using System.Globalization;
+using BusinessLayer.Container; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IServiceService, ServiceManager>();
-builder.Services.AddScoped<IServiceDal, EFServiceDal>();
-builder.Services.AddScoped<ITeamService, TeamManager>();
-builder.Services.AddScoped<ITeamDal, EFTeamDal>();
-builder.Services.AddScoped<IAnnouncementService, AnnouncementManager>();
-builder.Services.AddScoped<IAnnouncementDal, EFAnnouncementDal>();
-builder.Services.AddScoped<IImageService, ImageManager>();
-builder.Services.AddScoped<IImageDal, EFImageDal>();
-builder.Services.AddScoped<IAddressService, AddressManager>();
-builder.Services.AddScoped<IAddressDal, EFAddressDal>();
-builder.Services.AddScoped<IContactService, ContactManager>();
-builder.Services.AddScoped<IContactDal, EFContactDal>();
-builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
-builder.Services.AddScoped<ISocialMediaDal, EFSocialMediaDal>();
-builder.Services.AddScoped<IAdminService, AdminManager>();
-builder.Services.AddScoped<IAdminDal, EFAdminDal>();
+
 builder.Services.AddDbContext<AgricultureContext>();
 
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AgricultureContext>();
+
+ builder.Services.ContainerDependencies();
 
 
 // Add services to the container.
---
+++ BusinessLayer/Container/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Container
+{
+    public static class Extensions
+    {
+        public static void ContainerDependencies(this IServiceCollection services)
+        {
+           services.AddScoped<IServiceService, ServiceManager>();
+           services.AddScoped<IServiceDal, EFServiceDal>();
+            services.AddScoped<ITeamService, TeamManager>();
+            services.AddScoped<ITeamDal, EFTeamDal>();
+            services.AddScoped<IAnnouncementService, AnnouncementManager>();
+            services.AddScoped<IAnnouncementDal, EFAnnouncementDal>();
+            services.AddScoped<IImageService, ImageManager>();
+            services.AddScoped<IImageDal, EFImageDal>();
+            services.AddScoped<IAddressService, AddressManager>();
+            services.AddScoped<IAddressDal, EFAddressDal>();
+            services.AddScoped<IContactService, ContactManager>();
+            services.AddScoped<IContactDal, EFContactDal>();
+            services.AddScoped<ISocialMediaService, SocialMediaManager>();
+            services.AddScoped<ISocialMediaDal, EFSocialMediaDal>();
+            services.AddScoped<IAdminService, AdminManager>();
+            services.AddScoped<IAdminDal, EFAdminDal>();
+        }
+    }
+}
