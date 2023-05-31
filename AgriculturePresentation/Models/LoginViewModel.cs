bd196b243ed84a413b12aa283b5f8e0c928b8b24
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-05-31 12:39:22 +0300

    Tüm sayfalara vazgeç butonu eklendi.

--- AgriculturePresentation/Models/LoginViewModel.cs
+++ AgriculturePresentation/Models/LoginViewModel.cs
@@ -4,9 +4,10 @@ namespace AgriculturePresentation.Models
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage ="Lütfen kullanıcı adını giriniz")]
+        [Required(ErrorMessage = "Lütfen kullanıcı adını giriniz")]
         public string username { get; set; }
-        [Required(ErrorMessage="Lütfen şifreyi giriniz")]
+        [Required(ErrorMessage = "Lütfen şifreyi giriniz")]
         public string password { get; set; }
+       
     }
 }
